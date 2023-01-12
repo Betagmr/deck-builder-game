f39563162760f276ba3c02618e93f8b2a14dc530
Asier Villar <asiervillar@outlook.es>
2023-01-12 17:46:19 +0100

    ADD: settings

---
+++ settings.py
@@ -0,0 +1,15 @@
+# Settings for the game
+WINDOW_TITLE = "My Game"
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+# Fonts
+NORMAL_TEXT = "Arial"
+NORMAL_TEXT_BOLD = "Arial Bold"
+NORMAL_TEXT_SIZE = 20
+
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# Path
